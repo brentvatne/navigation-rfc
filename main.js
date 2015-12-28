e431bb0d9c9633567d48f6cd7351a7589cfe4d61
Brent Vatne <brentvatne@gmail.com>
2015-12-28 13:24:07 -0800

    Add slow push example to test Animated work

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const {
 
 // const MainApp = require('./Examples/NavigationBasicExample');
 // const MainApp = require('./Examples/NavigationContainerExample');
-const MainApp = require('./Examples/NavigationAnimatedExample');
+// const MainApp = require('./Examples/NavigationAnimatedExample');
+const MainApp = require('./Examples/NavigationJankyExample');
 
 AppRegistry.registerComponent('main', () => MainApp);
